41f21d712409828f2f25c298a1121f62486ea503
Philipp0205 <36603238+Philipp0205@users.noreply.github.com>
2026-08-29 15:24:12 +0000

    Add the accessible reader: topics, key points, read-aloud, big type

--- src/main/java/com/kindlerss/web/AccountAdvice.java
+++ src/main/java/com/kindlerss/web/AccountAdvice.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 /** Publishes the signed-in account's e-mail and verification status to every view. */
-@ControllerAdvice(assignableTypes = {AppController.class, SettingsController.class, AdminController.class})
+@ControllerAdvice(assignableTypes = {AppController.class, SettingsController.class, AdminController.class,
+        AccessibleController.class})
 public class AccountAdvice {
 
     private final CurrentUser currentUser;
